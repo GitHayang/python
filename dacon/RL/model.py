f32c89443e8a76d0d58843ae347b984a56119f80
GitHayang <127068766+GitHayang@users.noreply.github.com>
2024-02-13 09:07:27 +0900

    Add files via upload

---
+++ dacon/RL/model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+
+# In[2]:
+
+
+class Linear_QNet(nn.Module):
+    def __init(self):
+        pass
+    
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+
+# In[ ]:
+
+
+
+
